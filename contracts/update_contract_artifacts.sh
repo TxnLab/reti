c80fcfaa0c7e933f717cb20e72a4ffaa9d506732
Doug Richar <drichar@gmail.com>
2024-03-06 15:59:49 -0500

    chore: update build script to cp clients to ui

--- contracts/update_contract_artifacts.sh
+++ contracts/update_contract_artifacts.sh
@@ -3,3 +3,8 @@ rm -rf ../nodemgr/internal/lib/reti/artifacts/contracts/
 set -e
 mkdir -p ../nodemgr/internal/lib/reti/artifacts/contracts/
 cp ./contracts/artifacts/*arc32* ../nodemgr/internal/lib/reti/artifacts/contracts/
+
+# Update UI contract clients
+rm -rf ../ui/src/contracts/
+mkdir -p ../ui/src/contracts/
+cp ./contracts/clients/*.ts ../ui/src/contracts/
