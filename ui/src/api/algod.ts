f6f9fe71e330b3bfc65d0a55a53298c591cbf629
Doug Richar <drichar@gmail.com>
2024-03-13 04:26:59 -0400

    feat(ui): add stake validation accounts for wallet balance

---
+++ ui/src/api/algod.ts
@@ -0,0 +1,27 @@
+import * as algokit from '@algorandfoundation/algokit-utils'
+import { getAlgodConfigFromViteEnvironment } from '@/utils/network/getAlgoClientConfigs'
+import { AccountInformation, Exclude } from '@/interfaces/algod'
+
+const algodConfig = getAlgodConfigFromViteEnvironment()
+const algodClient = algokit.getAlgoClient({
+  server: algodConfig.server,
+  port: algodConfig.port,
+  token: algodConfig.token,
+})
+
+export async function getAccountInformation(
+  address: string,
+  exclude: Exclude = 'none',
+): Promise<AccountInformation> {
+  const accountInfo = await algodClient.accountInformation(address).exclude(exclude).do()
+  return accountInfo as AccountInformation
+}
+
+export async function getAccountBalance(
+  address: string,
+  availableBalance = false,
+): Promise<number> {
+  const accountInfo = await getAccountInformation(address, 'all')
+
+  return availableBalance ? accountInfo.amount - accountInfo['min-balance'] : accountInfo.amount
+}
