85fce279b35e80b5898d94786568b728d6cb36cc
Patrick Bennett <patrick+git@bennettfamily.org>
2024-04-02 18:12:05 -0400

    Move logos to public/img Remove unused assets/logo.svg

--- ui/src/components/Layout.tsx
+++ ui/src/components/Layout.tsx
@@ -5,18 +5,16 @@ import { ConnectedMenu } from '@/components/ConnectedMenu'
 import { MobileMenu } from '@/components/MobileMenu'
 import { ModeToggle } from '@/components/ModeToggle'
 import { Navigation } from '@/components/Navigation'
-import logoblack from '@/assets/logoblack.svg';
-import logowhite from '@/assets/logowhite.svg';
-import { useTheme } from '@/providers/ThemeProvider';
+import { useTheme } from '@/providers/ThemeProvider'
 
 interface LayoutProps {
   title?: string
   children: React.ReactNode
 }
 
-export function Layout({children}: LayoutProps) {
-  const {activeAddress} = useWallet()
-  const {theme} = useTheme()
+export function Layout({ children }: LayoutProps) {
+  const { activeAddress } = useWallet()
+  const { theme } = useTheme()
 
   return (
     <div className="min-h-full">
@@ -25,21 +23,24 @@ export function Layout({children}: LayoutProps) {
           <div className="flex h-16 justify-between">
             <div className="flex">
               <div className="-ml-2 mr-2 flex items-center md:hidden">
-                <MobileMenu/>
+                <MobileMenu />
               </div>
               <div className="flex flex-shrink-0 items-center">
-                <Link to="/" activeOptions={{exact: true}}>
-                  <img src={theme === 'dark' ? logowhite : logoblack} className="h-8 w-auto"/>
+                <Link to="/" activeOptions={{ exact: true }}>
+                  <img
+                    src={theme === 'dark' ? '/img/logowhite.svg' : '/img/logoblack.svg'}
+                    className="h-8 w-auto"
+                  />
                 </Link>
               </div>
               <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
-                <Navigation/>
+                <Navigation />
               </div>
             </div>
             <div className="flex items-center gap-x-2">
-              <ModeToggle/>
+              <ModeToggle />
               <div className="flex-shrink-0">
-                {activeAddress ? <ConnectedMenu activeAddress={activeAddress}/> : <Connect/>}
+                {activeAddress ? <ConnectedMenu activeAddress={activeAddress} /> : <Connect />}
               </div>
             </div>
           </div>
