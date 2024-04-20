5c59ec55e517a263d7dc7d140cd468c711b2267e
Doug Richar <drichar@gmail.com>
2024-04-20 00:59:17 -0400

    feat(ui): add findPoolForStaker to API functions

--- ui/src/api/contracts.ts
+++ ui/src/api/contracts.ts
@@ -10,6 +10,7 @@ import { StakedInfo, StakerPoolData, StakerValidatorData } from '@/interfaces/st
 import {
   Constraints,
   EntryGatingAssets,
+  FindPoolForStakerResponse,
   MbrAmounts,
   NodePoolAssignmentConfig,
   PoolInfo,
@@ -557,6 +558,50 @@ export async function doesStakerNeedToPayMbr(
     .simulate({ allowEmptySignatures: true, allowUnnamedResources: true })
 
   return result.returns![0]
+}
+
+export async function findPoolForStaker(
+  validatorId: number,
+  amountToStake: number,
+  activeAddress: string,
+  authAddr?: string,
+  client?: ValidatorRegistryClient,
+): Promise<FindPoolForStakerResponse> {
+  const validatorClient = client || makeSimulateValidatorClient(activeAddress, authAddr)
+
+  const result = await validatorClient
+    .compose()
+    .gas({})
+    .findPoolForStaker(
+      {
+        validatorId,
+        staker: activeAddress,
+        amountToStake,
+      },
+      {
+        sendParams: {
+          fee: AlgoAmount.MicroAlgos(2000),
+        },
+      },
+    )
+    .simulate({ allowEmptySignatures: true, allowUnnamedResources: true })
+
+  const errorMessage = result.simulateResponse.txnGroups[0].failureMessage
+
+  if (errorMessage || !result.returns[1]) {
+    throw new Error(`Error finding pool for staker: ${errorMessage || 'No pool found'}`)
+  }
+
+  const [[valId, poolId, poolAppId], isNewStakerToValidator, isNewStakerToProtocol] =
+    result.returns[1]
+
+  const poolKey: ValidatorPoolKey = {
+    validatorId: Number(valId),
+    poolId: Number(poolId),
+    poolAppId: Number(poolAppId),
+  }
+
+  return { poolKey, isNewStakerToValidator, isNewStakerToProtocol }
 }
 
 export async function addStake(
