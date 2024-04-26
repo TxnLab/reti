146fda8d69b38ad4d8c74c2e4e2839311a64c86e
Doug Richar <drichar@gmail.com>
2024-04-26 04:55:56 -0400

    test(ui): intercept algod requests with Mock Service Worker
    Since the app clients making simulate calls expect a functioning algod client as an argument, mocking algosdk.Algodv2 isn't an option.
    
    MSW will let us intercept the actual http simulate requests to fetch data and mock their responses.
    
    Now we can properly isolate components for testing and provide fixture data for predictable, repeatable outcomes.

---
+++ ui/src/utils/tests/fixtures/methods.ts
@@ -0,0 +1,27 @@
+import { AlgoAmount } from '@algorandfoundation/algokit-utils/types/amount'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type FixtureFunction = (args: any) => any[]
+
+// Map containing each ABI method's corresponding fixture function
+export const methodFixtures: Record<string, FixtureFunction> = {
+  getPools: ({ validatorId }: { validatorId: number | bigint }) => {
+    const pool1 = {
+      appId: 1010,
+      balance: AlgoAmount.Algos(10_000_000).microAlgos,
+    }
+    const pool2 = {
+      appId: 1020,
+      balance: AlgoAmount.Algos(250_000).microAlgos,
+    }
+
+    switch (Number(validatorId)) {
+      case 1:
+        return [[BigInt(pool1.appId), BigInt(validatorId), BigInt(pool1.balance)]]
+      case 2:
+        return [[BigInt(pool2.appId), BigInt(validatorId), BigInt(pool2.balance)]]
+      default:
+        return [[0n, 0n, 0n]]
+    }
+  },
+}
