71638a9030bb4e9d710540024b2e541d358d1fe5
Doug Richar <drichar@gmail.com>
2024-05-23 12:05:24 -0400

    chore(ui): update add validator form token payout conversion

--- ui/src/components/AddValidatorForm.tsx
+++ ui/src/components/AddValidatorForm.tsx
@@ -249,11 +249,15 @@ export function AddValidatorForm({ constraints }: AddValidatorFormProps) {
         throw new Error('No active address')
       }
 
+      if (!rewardToken) {
+        throw new Error('No reward token entered')
+      }
+
       toast.loading('Sign transactions to add validator...', { id: toastId })
 
       const rewardPerPayout = convertToBaseUnits(
-        Number(values.rewardPerPayout),
-        Number(rewardToken?.params.decimals || 0),
+        values.rewardPerPayout,
+        rewardToken.params.decimals,
       )
 
       const epochRoundLength = getEpochLengthBlocks(
