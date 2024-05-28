354ec158d08b869099e5514c5f684797e30aace3
Doug Richar <drichar@gmail.com>
2024-05-28 17:56:56 -0400

    fix(ui): pressing Enter should submit add/remove stake form (#193)

--- ui/src/components/UnstakeModal.tsx
+++ ui/src/components/UnstakeModal.tsx
@@ -302,6 +302,7 @@ export function UnstakeModal({ validator, setValidator, stakesByValidator }: Uns
                           <div className="relative">
                             <Input className="pr-16" {...field} />
                             <Button
+                              type="button"
                               size="sm"
                               variant="outline"
                               className="absolute inset-y-1 right-1.5 h-7 px-2 flex items-center text-muted-foreground text-xs uppercase"
