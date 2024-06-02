6825d661f3b32fab2555b2e345fd4c0559697dd0
Doug Richar <drichar@gmail.com>
2024-06-02 13:23:47 -0400

    wip: test custom plugin

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
-import replace from '@rollup/plugin-replace'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 import path from 'path'
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
@@ -9,11 +9,18 @@ import { version } from './package.json'
 
 export default defineConfig({
   plugins: [
-    replace({
-      __APP_VERSION__: JSON.stringify(version),
-      preventAssignment: true,
-      include: ['public/version.json'],
-    }),
+    {
+      name: 'replace-version-in-json',
+      apply: 'build',
+      enforce: 'pre',
+      generateBundle() {
+        const filePath = path.resolve(__dirname, 'public/version.json')
+        const content = fs.readFileSync(filePath, 'utf-8')
+        const updatedContent = content.replace('__APP_VERSION__', version)
+        const newFilePath = path.resolve(__dirname, 'dist/version.json')
+        fs.writeFileSync(newFilePath, updatedContent, 'utf-8')
+      },
+    },
     react(),
     TanStackRouterVite(),
     nodePolyfills({
